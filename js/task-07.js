17a1d68c432d7370655c64eec6d5b2dbbf07a480
Roman Lysenko <romashkaxxxl@gmail.com>
2022-08-27 19:57:44 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
-document.querySelector('#font-size-control').oninput = function () {
-
-    if (this.value >= this.getAttribute('min') && this.value <= this.getAttribute('max')) {
-        document.querySelector('#text').style.fontSize = ${ this.value } px;
-    };
-}
+document.querySelector("#font-size-control").oninput = function () {
+  if (
+    this.value >= this.getAttribute("min") &&
+    this.value <= this.getAttribute("max")
+  ) {
+    document.querySelector("#text").style.fontSize = `${this.value} px`;
+  }
+};
