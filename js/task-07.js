5848718ab626498df21270968d85da1d56d7fb19
Roman Lysenko <romashkaxxxl@gmail.com>
2022-08-27 20:21:27 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,23 @@
+// Напиши скрипт, який реагує на зміну значення input#font - size - control(подія input) і
+// змінює інлайн - стиль span#text, оновлюючи властивість font - size.В результаті,
+//  перетягуючи повзунок,
+//     буде змінюватися розмір тексту.
+
 document.querySelector("#font-size-control").oninput = function () {
   if (
     this.value >= this.getAttribute("min") &&
     this.value <= this.getAttribute("max")
   ) {
-    document.querySelector("#text").style.fontSize = `${this.value} px`;
+    document.querySelector("#text").style.fontSize = `${this.value}px`;
   }
 };
+
+// const asp = document.querySelector("#font-size-control");
+// asp.addEventListener("input", () => {
+//   if (
+//     this.value >= this.getAttribute("min") &&
+//     this.value <= this.getAttribute("max")
+//   ) {
+//     document.querySelector("#text").style.fontSize = `${this.value} px`;
+//   }
+// });
