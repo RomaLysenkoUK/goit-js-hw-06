db92f8552bebbdbb92fb8a630f19797a7747eaa2
Roman Lysenko <romashkaxxxl@gmail.com>
2022-08-27 20:33:42 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,21 +3,21 @@
 //  перетягуючи повзунок,
 //     буде змінюватися розмір тексту.
 
-document.querySelector("#font-size-control").oninput = function () {
-  if (
-    this.value >= this.getAttribute("min") &&
-    this.value <= this.getAttribute("max")
-  ) {
-    document.querySelector("#text").style.fontSize = `${this.value}px`;
-  }
-};
-
-// const asp = document.querySelector("#font-size-control");
-// asp.addEventListener("input", () => {
+// document.querySelector("#font-size-control").oninput = function () {
 //   if (
 //     this.value >= this.getAttribute("min") &&
 //     this.value <= this.getAttribute("max")
 //   ) {
-//     document.querySelector("#text").style.fontSize = `${this.value} px`;
+//     document.querySelector("#text").style.fontSize = `${this.value}px`;
 //   }
-// });
+// };
+
+const asp = document.querySelector("#font-size-control");
+asp.addEventListener("input", (event) => {
+  if (
+    event.target.value >= event.target.getAttribute("min") &&
+    event.target.value <= event.target.getAttribute("max")
+  ) {
+    document.querySelector("#text").style.fontSize = `${event.target.value}px`;
+  }
+});
